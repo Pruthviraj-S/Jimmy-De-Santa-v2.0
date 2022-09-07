c07a54662ffc9258882a4e924fbcf520b3fbba22
PruthvirajS <62303937+Pruthviraj-S@users.noreply.github.com>
2022-09-08 00:57:41 +0530

    Update Message event,nuub mistake

--- Events/Message/messageCreate.js
+++ Events/Message/messageCreate.js
@@ -10,12 +10,8 @@ module.exports = {
      * @param {Message} message 
      */
     async execute(message, client) {
-        // #rhts-crew-submissions react
-        if (message.channelId === '889513184686383134') {
-            await message.react('<:yes:882852788101607494>');
-            message.react('<:no:882852872574869515>');
-        }
-        else if (message.channelId === '988832045369278464') {
+        // #rhts-crew-submissions,car meet react
+        if (message.channelId === '889513184686383134' || '988832045369278464') {
             await message.react('<:yes:882852788101607494>');
             message.react('<:no:882852872574869515>');
         }
