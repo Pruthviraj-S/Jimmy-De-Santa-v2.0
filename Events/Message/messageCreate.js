0b917841add6fea88b11606017c5493cd972c27d
PruthvirajS <62303937+Pruthviraj-S@users.noreply.github.com>
2022-09-08 00:36:38 +0530

    Update Message event

--- Events/Message/messageCreate.js
+++ Events/Message/messageCreate.js
@@ -1,5 +1,5 @@
 const { dbtoken, guildid } = require('../../config.json');
-const { Client,Message} = require('discord.js')
+const { Client, Message } = require('discord.js')
 const { mongoose } = require('mongoose')
 
 module.exports = {
@@ -9,9 +9,13 @@ module.exports = {
      * @param {Client} client 
      * @param {Message} message 
      */
-    async execute( message,client) {
+    async execute(message, client) {
         // #rhts-crew-submissions react
         if (message.channelId === '889513184686383134') {
+            await message.react('<:yes:882852788101607494>');
+            message.react('<:no:882852872574869515>');
+        }
+        else if (message.channelId === '988832045369278464') {
             await message.react('<:yes:882852788101607494>');
             message.react('<:no:882852872574869515>');
         }
