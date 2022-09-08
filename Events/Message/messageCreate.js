06f7a6b760d19caea6e462228f5e94f89936ac03
PruthvirajS <62303937+Pruthviraj-S@users.noreply.github.com>
2022-09-08 08:46:42 +0530

    Added thread check,attach

--- Events/Message/messageCreate.js
+++ Events/Message/messageCreate.js
@@ -10,7 +10,7 @@ module.exports = {
     async execute(message, client) {
         // #rhts-crew-submissions,car meet react
         if (message.channelId === '889513184686383134' || message.channel.parentId === '988832045369278464') {
-            if (message.attachments > 0) {
+            if (message.attachments.size > 0) {
                 await message.react('<:yes:882852788101607494>');
                 message.react('<:no:882852872574869515>');
             }
